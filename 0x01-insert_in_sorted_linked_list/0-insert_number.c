534c83bdc33b5a6289cb562f429de8f972dbc1a1
justinmajetich <justinmajetich@gmail.com>
2020-07-28 11:06:14 -0400

    Update header file

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,6 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 
 listint_t *insert_node(listint_t **head, int number)
 {
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,2 @@
+# 0x01. Insert in sorted linked list
+A program to insert a number into a sorted singly-linked list
