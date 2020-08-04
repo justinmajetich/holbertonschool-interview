456e41d24ee517ceba878761cb4b6b8ba4996ac8
justinmajetich <justinmajetich@gmail.com>
2020-08-04 16:44:06 -0400

    Create function to add new node to binary tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node = NULL;
+
+    /* If parent node is NULL */
+    if (!parent)
+    {
+        return (NULL);
+    }
+
+    /* Allocate and assign space for new node */
+    new_node = malloc(sizeof(binary_tree_t));
+    if (!new_node)
+    {
+        return (NULL);
+    }
+    
+    /* Initialize members of new node */
+    new_node->parent = parent;
+    new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return new_node;
+}
