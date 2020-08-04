578c2bf18dadce7ba99ff833bb3e5af96113daef
justinmajetich <justinmajetich@gmail.com>
2020-08-04 16:57:27 -0400

    Replace spaces with tabs

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,20 +8,21 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-    /* Allocate and assign space for new node */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-    {
-        return (NULL);
-    }
+	/* Allocate and assign space for new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	{
+ 		turn (NULL);
+	}
 
-    /* Initialize members of new node */
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	/* Initialize members of new node */
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return new_node;
+	return new_node;
 }
+
