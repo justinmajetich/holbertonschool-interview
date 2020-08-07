f68f8fbdf571fe46764e3f32b5cb61513125004f
justinmajetich <justinmajetich@gmail.com>
2020-08-07 20:10:50 +0000

    Calculate number of copy/paste operations needed to achieve character sequence of n length

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """ Calculate the minimun operations necessary to copy/paste
+        given character to appear n times.
+    """
+    operationCount = 0
+    _file = 'H'
+    buffer = ''
+
+    while len(_file) < n:
+
+        # If target size n is cleaning divisible by current size
+        if n % len(_file) == 0:
+            # Copy file contents to buffer
+            buffer = _file
+            operationCount += 1
+
+        # Paste buffer to file
+        _file += buffer
+        operationCount += 1
+
+    return operationCount
