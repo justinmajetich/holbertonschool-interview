f31358ecf52c787ffc1a847d8ce781cc71fc254d
justinmajetich <justinmajetich@gmail.com>
2020-08-21 19:02:09 +0000

    Fix styling issues

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -5,38 +5,38 @@ int is_equal(listint_t *left, listint_t *right);
 /**
  * is_palindrome - Detects if the contents of linked list are palindromic
  * @head: Head node of list
- * 
+ *
  * Return: 0 if not a palindrome; 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
-    /* If list is empty, consider it a palindrome */
-    if (*head == NULL)
-        return (1);
+	/* If list is empty, consider it a palindrome */
+	if (*head == NULL)
+		return (1);
 
     /* Check if list is palindrome */
-    return is_equal(*head, *head);
+	return (is_equal(*head, *head));
 }
 
 /**
- * is_equal - Recursively check if symmetric nodes are of equal value 
+ * is_equal - Recursively check if symmetric nodes are of equal value
  * @left: Node probing from the left
  * @right: Node probing from the right
- * 
+ *
  * Return: 1 if values match; otherwise, 0
  */
 int is_equal(listint_t *left, listint_t *right)
 {
-    /* Base case is end of list */
-    if (right == NULL)
-        return (1);
+	/* Base case is end of list */
+	if (right == NULL)
+		return (1);
 
-    /* Check if nodes have equal value */
-    if (!is_equal(left, right->next))
-        return (0);
+	/* Check if nodes have equal value */
+	if (!is_equal(left, right->next))
+		return (0);
 
-    /* Move left pointer forward */
-    left = left->next;
+	/* Move left pointer forward */
+	left = left->next;
 
-    return (1);
+	return (1);
 }
