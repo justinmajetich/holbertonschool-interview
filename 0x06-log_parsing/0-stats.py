afde1cc10f923a177f8c55c7a37f4a0aa3f888a5
justinmajetich <justinmajetich@gmail.com>
2020-09-01 23:54:10 +0000

    Remove unnecessary guards

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -30,22 +30,18 @@ loopCounter = 0
 try:
     for line in stdin:
         # Pull necessary fields from log line
-        lineSplit = line.split()
+        statusCode, fileSize = [part for part in line.split()[-2:]]
 
-        if len(lineSplit) >= 2:
-            statusCode, fileSize = [part for part in lineSplit[-2:]]
+        # Update persistent size and status counters
+        totalFileSize += int(fileSize)
+        codeTracker[statusCode] += 1
 
-            # Update persistent size and status counters
-            totalFileSize += int(fileSize)
-            if statusCode in codeTracker:
-                codeTracker[statusCode] += 1
-
-                # Keep track of how many logs have been read in print loop
-                if loopCounter == 9:
-                    printCodeTracking(totalFileSize, codeTracker)
-                    loopCounter = 0
-                else:
-                    loopCounter += 1
+        # Keep track of how many logs have been read in print loop
+        if loopCounter == 9:
+            printCodeTracking(totalFileSize, codeTracker)
+            loopCounter = 0
+        else:
+            loopCounter += 1
 
     # Print stats at end of input stream
     printCodeTracking(totalFileSize, codeTracker)
