27f3aa39395de7806237a4eadd5cc4ac8b112cd3
justinmajetich <justinmajetich@gmail.com>
2020-09-07 02:36:01 +0000

    Fix style errors

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -10,15 +10,22 @@ int run_cycle_check(listint_t *slow, listint_t *fast);
  */
 int check_cycle(listint_t *list)
 {
-    return run_cycle_check(list->next, list->next->next);
+	return (run_cycle_check(list->next, list->next->next));
 }
 
+/**
+ * run_cycle_check - Recursively run slow and fast nodes through list.
+ * @slow: Slow node
+ * @fast: Fast node
+ *
+ * Return: 1 if list contains cycle; otherwise, 0.
+ */
 int run_cycle_check(listint_t *slow, listint_t *fast)
 {
-    if (!slow || !fast)
-        return (0);
-    else if (fast == slow)
-        return (1);
-    else
-        return run_cycle_check(slow->next, fast->next->next);
+	if (!slow || !fast)
+		return (0);
+	else if (fast == slow)
+		return (1);
+	else
+		return (run_cycle_check(slow->next, fast->next->next));
 }
