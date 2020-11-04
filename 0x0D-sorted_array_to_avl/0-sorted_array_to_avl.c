f0b7e8fb8d2628f692aef959617eab2caf33b967
justinmajetich <justinmajetich@gmail.com>
2020-11-04 19:52:13 +0000

    Create header file

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - Generate an AVL tree from a sorted array.
+ * @array: Array from which to generate tree.
+ * @size: Size of array.
+ * 
+ * Return: Pointer to root node of AVL tree; NULL on failure.
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+
+}
