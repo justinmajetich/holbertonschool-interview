520ee69a13ded399e0757e177a2eae4b2e0092e1
justinmajetich <justinmajetich@gmail.com>
2020-11-08 17:28:32 +0000

    Fix styling errors

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -17,10 +17,12 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
