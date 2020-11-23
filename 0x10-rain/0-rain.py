5dfd3d225dcab16a8abfdc19bdb4354082cf03ea
justinmajetich <justinmajetich@gmail.com>
2020-11-23 21:23:44 +0000

    Re-work into double pass algorithm

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,42 +4,40 @@
 """
 
 
-def find_volume_between_walls(walls, left, right):
-    """
-        Find the number of units that may be contained between walls.
-    """
-    left_wall = walls[left]
-    right_wall_index = 0
-    filled_space = 0
-    width = 0
-
-    for i in range(left + 1, right):
-        if walls[i] < left_wall:
-            filled_space += walls[i]
-        else:
-            right_wall_index = i
-            break
-        width += 1
-
-    return [(left_wall * width) - filled_space, right_wall_index]
-
-
 def rain(walls):
     """
         Determine how many units of rain can be contained within array's walls.
     """
-    totlal_volume = 0
+    water_temp = 0
+    water = 0
     size = len(walls)
+    prev_wall = 0
 
     # If list is empty return 0.
     if size <= 0:
-        return totlal_volume
+        return water_temp
 
     # Iterate through list to find walls.
     for i in range(size):
-        if walls[i] >= 1:
-            volume_data = find_volume_between_walls(walls, i, size)
-            totlal_volume += volume_data[0]
-            i = volume_data[1] - 1
 
-    return totlal_volume
+        if walls[i] >= walls[prev_wall]:
+            prev_wall = i
+            water_temp = 0
+        else:
+            water += walls[prev_wall] - walls[i]
+            water_temp += walls[prev_wall] - walls[i]
+
+    if prev_wall < size - 1:
+
+        water -= water_temp
+        prev_pass_peak = prev_wall
+        prev_wall = size - 1
+
+        for i in range(size - 1, prev_pass_peak, -1):
+
+            if walls[i] >= walls[prev_wall]:
+                prev_wall = i
+            else:
+                water += walls[prev_wall] - walls[i]
+
+    return water
