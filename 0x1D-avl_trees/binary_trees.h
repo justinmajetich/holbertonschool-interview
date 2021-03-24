0f9791b70dfa81d28a0ceb0354b0d96105df1f6d
justinmajetich <justinmajetich@gmail.com>
2021-03-24 14:59:35 +0000

    Add prototypes

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,6 +1,10 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,5 +24,9 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 typedef struct binary_tree_s avl_t;
+
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 #endif
